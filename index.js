8aa5d43090e15822437a147a70e64b7e8231bf65
wujiank <wujiank@yonyou.com>
2018-03-15 19:42:37 +0800

    修改静态资源托管问题

--- index.js
+++ index.js
@@ -26,6 +26,28 @@ try {
 function server() {
   //设置默认mock
   app.use(express.static(path.resolve('.', 'mock')));
+  /* 若是与webpack结合使用，配置了webpack则启动webpack-hot-middleware与webpack-dev-middleware搭建的热替换server
+   * 若是与gulp结合使用，则利用express静态资源托管，由gulp去管理文件的监控
+   * 先做静态资源托管，不然可能会与代理冲突出现的问题
+  */
+  console.log(chalk.yellow("\n/******************** Start webpack-dev-middleware *****************/\n"))
+  if (svrConfig.buildTool=="webpack") {
+    //加载webpack处理
+    var webpackConfig = require(path.resolve(".", svrConfig.webpackCfgName));
+    var compiler = webpack(webpackConfig);
+    console.log(chalk.yellow("\n/******************** Start webpack-dev-middleware *****************/\n"))
+    app.use(webpackDevMiddleware(compiler, {
+      publicPath: webpackConfig.output.publicPath,//资源目录与插件做绑定，与webpack配置的一致
+      logTime: true,
+      stats: {
+        colors: true
+      }
+    }));
+    app.use(webpackHotMiddleware(compiler));
+  } else {
+    //设置指定静态资源目录
+    app.use(svrConfig.proName,express.static(path.resolve('.', svrConfig.staticFolder)));
+  }
   console.log(chalk.yellow("\n/******************** Start loading proxy server ********************/\n"));
   proxyConfig.forEach(function (element) {
     if (element.enable) {
@@ -54,27 +76,6 @@ function server() {
     console.log(chalk.yellow("\n/******************** Mock server loaded completed *****************/\n"));
   }
   app.use(router);
-  /* 若是与webpack结合使用，配置了webpack则启动webpack-hot-middleware与webpack-dev-middleware搭建的热替换server
-   * 若是与gulp结合使用，则利用express静态资源托管，由gulp去管理文件的监控
-  */
-  console.log(chalk.yellow("\n/******************** Start webpack-dev-middleware *****************/\n"))
-  if (svrConfig.buildTool=="webpack") {
-    //加载webpack处理
-    var webpackConfig = require(path.resolve(".", svrConfig.webpackCfgName));
-    var compiler = webpack(webpackConfig);
-    console.log(chalk.yellow("\n/******************** Start webpack-dev-middleware *****************/\n"))
-    app.use(webpackDevMiddleware(compiler, {
-      publicPath: webpackConfig.output.publicPath,//资源目录与插件做绑定，与webpack配置的一致
-      logTime: true,
-      stats: {
-        colors: true
-      }
-    }));
-    app.use(webpackHotMiddleware(compiler));
-  } else {
-    //设置指定静态资源目录
-    app.use(svrConfig.proName,express.static(path.resolve('.', svrConfig.staticFolder)));
-  }
   app.listen(svrConfig.port, svrConfig.host, function () {
     console.log(chalk.yellow("\n/******************** Start dev server *****************/\n"));
     console.log(chalk.green(`[pmm] : Listening on port http://${svrConfig.host}:${svrConfig.port}`));
